69239642a552bd5db865ef82a01818568095edf9
sun14317 <llam29@ssis.edu.vn>
2026-05-21 08:30:02 +0700

    Update picture magenta.py
    This is the magenta triangles part of the code

--- picture.py
+++ picture.py
@@ -36,6 +36,11 @@ def draw_picture(width, height):
  
  
     # code for MAGENTA triangles
+    sg.set_fill_color(colors[4]) # set fill color to red
+    
+    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
+        sg.fill_triangle(0*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 1*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 1*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
+        
     
     
     # code for CYAN triangles
